de7aab28cbb18c71c1b80d06dac483a14220c4d7
Simone Massaro <49060178+simone7687@users.noreply.github.com>
2022-06-17 18:42:04 +0200

    varius Improvement

--- reactinferface/src/App.tsx
+++ reactinferface/src/App.tsx
@@ -1,14 +1,42 @@
 // import your route components too
 
+import Editor from "@monaco-editor/react";
+import { AppBar, Box, CssBaseline, Toolbar, Typography } from "@mui/material";
 import Sidebars from "components/Sidebars";
 import ErrorBoundary from "errer_check/ErrorBoundary";
 import { BrowserRouter } from "react-router-dom";
+
+const drawerWidth = 240;
 
 function App() {
     return (
         <ErrorBoundary>
             <BrowserRouter>
-                <Sidebars />
+                <Box sx={{ display: 'flex' }}>
+                    <CssBaseline />
+                    <AppBar
+                        position="fixed"
+                        sx={{ width: `calc(100% - ${drawerWidth}px)`, ml: `${drawerWidth}px` }}
+                    >
+                        <Toolbar>
+                            <Typography variant="h6" noWrap component="div">
+                                Permanent drawer
+                            </Typography>
+                        </Toolbar>
+                    </AppBar>
+                    <Sidebars />
+                    <Box
+                        component="main"
+                        sx={{ flexGrow: 1, bgcolor: 'background.default', p: 3 }}
+                    >
+                        <Toolbar />
+                        <Editor
+                            height="90vh"
+                            defaultLanguage="sql"
+                            defaultValue="// some comment"
+                        />
+                    </Box>
+                </Box>
             </BrowserRouter>
         </ErrorBoundary>
     );
--- reactinferface/src/components/Sidebars.tsx
+++ reactinferface/src/components/Sidebars.tsx
@@ -1,56 +1,27 @@
-import Editor from "@monaco-editor/react";
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import CssBaseline from '@mui/material/CssBaseline';
 import Drawer from '@mui/material/Drawer';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
 import SideDatabase from './SideDatabase';
 
 const drawerWidth = 240;
 
 function Sidebars() {
     return (
-        <Box sx={{ display: 'flex' }}>
-            <CssBaseline />
-            <AppBar
-                position="fixed"
-                sx={{ width: `calc(100% - ${drawerWidth}px)`, ml: `${drawerWidth}px` }}
-            >
-                <Toolbar>
-                    <Typography variant="h6" noWrap component="div">
-                        Permanent drawer
-                    </Typography>
-                </Toolbar>
-            </AppBar>
-            <Drawer
-                sx={{
+        <Drawer
+            sx={{
+                width: drawerWidth,
+                flexShrink: 0,
+                '& .MuiDrawer-paper': {
                     width: drawerWidth,
-                    flexShrink: 0,
-                    '& .MuiDrawer-paper': {
-                        width: drawerWidth,
-                        boxSizing: 'border-box',
-                    },
-                }}
-                variant="permanent"
-                anchor="left"
-            >
-                <Toolbar />
-                <SideDatabase />
-                <SideDatabase />
-            </Drawer>
-            <Box
-                component="main"
-                sx={{ flexGrow: 1, bgcolor: 'background.default', p: 3 }}
-            >
-                <Toolbar />
-                <Editor
-                    height="90vh"
-                    defaultLanguage="sql"
-                    defaultValue="// some comment"
-                />
-            </Box>
-        </Box>
+                    boxSizing: 'border-box',
+                },
+            }}
+            variant="permanent"
+            anchor="left"
+        >
+            <Toolbar />
+            <SideDatabase />
+            <SideDatabase />
+        </Drawer>
     );
 }
 export default Sidebars;
