2a17650ae43cb643cf97f415a611f4dabd0ab812
Simone Massaro <49060178+simone7687@users.noreply.github.com>
2022-06-21 19:23:33 +0200

    add Cors connection

--- back-end/Program.cs
+++ back-end/Program.cs
@@ -1,4 +1,17 @@
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+
 var builder = WebApplication.CreateBuilder(args);
+
+// https://docs.microsoft.com/it-it/aspnet/core/security/cors?view=aspnetcore-6.0
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:3000");
+        });
+});
 
 // Add services to the container.
 
@@ -20,6 +33,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseAuthorization();
 
