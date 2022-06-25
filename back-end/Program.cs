c295b8154cc4c931a92f07784e713bc3eade5d6b
Simone Massaro <49060178+simone7687@users.noreply.github.com>
2022-06-25 11:12:55 +0200

    Add Allow Origin

--- back-end/Program.cs
+++ back-end/Program.cs
@@ -9,7 +9,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: MyAllowSpecificOrigins,
         policy =>
         {
-            policy.WithOrigins("http://localhost:3000")
+            policy.WithOrigins("http://localhost:3000", "https://database-management-syst-614c1.web.app")
                .AllowAnyMethod()
                .AllowAnyHeader();
         });
